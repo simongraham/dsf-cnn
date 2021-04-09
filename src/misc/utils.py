5ae17eb6a3d0bdafda78d318c1a8a2d3734340b8
simongraham <s.graham.1@warwick.ac.uk>
2021-04-09 09:20:16 +0100

    NEW: Added remap label to utils

--- src/misc/utils.py
+++ src/misc/utils.py
@@ -26,6 +26,8 @@ def bounding_box(img):
     rmax += 1
     cmax += 1
     return [rmin, rmax, cmin, cmax]
+
+
 ####
 
 
@@ -42,12 +44,14 @@ def cropping_center(img, crop_shape, batch=False):
     if not batch:
         h_0 = int((orig_shape[0] - crop_shape[0]) * 0.5)
         w_0 = int((orig_shape[1] - crop_shape[1]) * 0.5)
-        img = img[h_0:h_0 + crop_shape[0], w_0:w_0 + crop_shape[1]]
+        img = img[h_0 : h_0 + crop_shape[0], w_0 : w_0 + crop_shape[1]]
     else:
         h_0 = int((orig_shape[1] - crop_shape[0]) * 0.5)
         w_0 = int((orig_shape[2] - crop_shape[1]) * 0.5)
-        img = img[:, h_0:h_0 + crop_shape[0], w_0:w_0 + crop_shape[1]]
+        img = img[:, h_0 : h_0 + crop_shape[0], w_0 : w_0 + crop_shape[1]]
     return img
+
+
 ####
 
 
@@ -59,6 +63,8 @@ def rm_n_mkdir(dir_path):
     if os.path.isdir(dir_path):
         shutil.rmtree(dir_path)
     os.makedirs(dir_path)
+
+
 ####
 
 
@@ -70,7 +76,37 @@ def get_files(data_dir_list, data_ext):
 
     data_files = []
     for sub_dir in data_dir_list:
-        files = glob.glob(sub_dir + '/*' + data_ext)
+        files = glob.glob(sub_dir + "/*" + data_ext)
         data_files.extend(files)
 
     return data_files
+
+
+def remap_label(pred, by_size=False):
+    """Rename all instance id so that the id is contiguous i.e [0, 1, 2, 3] 
+    not [0, 2, 4, 6]. The ordering of instances (which one comes first) 
+    is preserved unless by_size=True, then the instances will be reordered
+    so that bigger object has smaller ID.
+    Args:
+        pred    : the 2d array contain instances where each instances is marked
+                  by non-zero integer
+        by_size : renaming with larger object has smaller id (on-top)
+    """
+    pred_id = list(np.unique(pred))
+    pred_id.remove(0)
+    if len(pred_id) == 0:
+        return pred  # no label
+    if by_size:
+        pred_size = []
+        for inst_id in pred_id:
+            size = (pred == inst_id).sum()
+            pred_size.append(size)
+        # sort the id by size in descending order
+        pair_list = zip(pred_id, pred_size)
+        pair_list = sorted(pair_list, key=lambda x: x[1], reverse=True)
+        pred_id, pred_size = zip(*pair_list)
+
+    new_pred = np.zeros(pred.shape, np.int32)
+    for idx, inst_id in enumerate(pred_id):
+        new_pred[pred == inst_id] = idx + 1
+    return new_pred
